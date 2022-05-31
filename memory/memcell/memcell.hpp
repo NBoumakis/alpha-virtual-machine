4c7b4768b76139b95be9476fa2fd09ad809954e2
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-05-31 15:16:25 +0300

    Base memcell

---
+++ memory/memcell/memcell.hpp
@@ -0,0 +1,22 @@
+enum memcell_type {
+    number_m,
+    string_m,
+    bool_m,
+    table_m,
+    userfunc_m,
+    libfunc_m,
+    nil_m,
+    undefined_m
+};
+
+class memcell {
+public:
+    memcell();
+    ~memcell();
+};
+
+memcell::memcell() {
+}
+
+memcell::~memcell() {
+}
