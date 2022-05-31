39134bbcc01dcbba9af15106236f2fa7c6013abe
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-05-31 20:05:26 +0300

    program stack contract

---
+++ memory/stack/program_stack.hpp
@@ -0,0 +1,21 @@
+#ifndef __PROGRAM_STACK_H
+#define __PROGRAM_STACK_H
+
+#include "memory/memcell/memcell.hpp"
+#include <deque>
+
+class program_stack {
+private:
+    std::deque<memcell *> stack;
+
+public:
+    program_stack();
+    ~program_stack();
+
+    void push(memcell *);
+    memcell *pop();
+
+    memcell *operator[](unsigned long);
+};
+
+#endif /* __PROGRAM_STACK_H */
