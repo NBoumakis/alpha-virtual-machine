546f60f6ff2a3bdde26e5e13d3af0d9a6de371cf
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-06-01 01:01:08 +0300

    Class execute implementation file

---
+++ executer/execute/execute.cpp
@@ -0,0 +1,9 @@
+#include "executer/execute/execute.hpp"
+#include "executer/execute/execute_funcs_arithmetic.hpp"
+#include "executer/execute/execute_funcs_assign.hpp"
+#include "executer/execute/execute_funcs_function.hpp"
+#include "executer/execute/execute_funcs_nop.hpp"
+#include "executer/execute/execute_funcs_relational.hpp"
+#include "executer/execute/execute_funcs_table.hpp"
+
+execute::execute() {}
