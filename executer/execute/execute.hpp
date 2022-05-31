f13e30f77f19455fac3e653d59ffa48b1f62e71c
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-05-31 23:36:42 +0300

    execute class definition

---
+++ executer/execute/execute.hpp
@@ -0,0 +1,19 @@
+#ifndef __EXECUTE_H
+#define __EXECUTE_H
+
+#include "lib/instruction.hpp"
+#include <functional>
+#include <unordered_map>
+
+class execute {
+private:
+    std::unordered_map<vmopcode, std::function<void(instruction *)>> dispatch;
+
+public:
+    execute();
+    ~execute();
+
+    std::function<void(instruction *)> operator[](vmopcode);
+};
+
+#endif /* __EXECUTE_H */
