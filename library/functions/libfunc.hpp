0859b77b3f0c990c1812166e9f9b99dc610b106f
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-06-01 12:58:58 +0300

    libfuncs

---
+++ library/functions/libfunc.hpp
@@ -0,0 +1,28 @@
+#ifndef __LIBFUNC_H
+#define __LIBFUNC_H
+
+void libfunc_typeof();
+
+void libfunc_print();
+
+void libfunc_totalarguments();
+
+void libfunc_input();
+
+void libfunc_objectmemberkeys();
+
+void libfunc_objecttotalmembers();
+
+void libfunc_objectcopy();
+
+void libfunc_argument();
+
+void libfunc_strtonum();
+
+void libfunc_sqrt();
+
+void libfunc_cos();
+
+void libfunc_sin();
+
+#endif /* __LIBFUNC_H */
