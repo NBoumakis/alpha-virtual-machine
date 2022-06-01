ba3319c85ede5a1ffc747a1246bfbf054853757d
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-06-01 18:31:22 +0300

    CPU class

---
+++ executer/cpu.hpp
@@ -0,0 +1,21 @@
+#include "library/environment/environment.hpp"
+#include "memory/code_segment/code_segment.hpp"
+#include "memory/const_values/const_values.hpp"
+#include "memory/memcell/memcell.hpp"
+#include "memory/stack/program_stack.hpp"
+
+class cpu {
+public:
+    static unsigned long pc, topsp, top, code_size;
+    static memcell *ax, *bx, *cx, *retval;
+    static program_stack stack;
+    static environment env;
+    static code_segment code;
+    static const_values pools;
+    static char execution_finished;
+
+    cpu();
+    ~cpu();
+
+    static void cycle();
+};
