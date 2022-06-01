2c7da2faca40473f063bc857bfe93957c8a93a7d
Konstantina <papafragakikonstantina@gmail.com>
2022-06-02 02:05:13 +0300

    implement destructor of dynamic_table

--- table/dynamic_table.cpp
+++ table/dynamic_table.cpp
@@ -3,3 +3,18 @@
 dynamic_table::dynamic_table(){
     refCounter=0;
 }
+
+dynamic_table::~dynamic_table(){
+    for (auto &memObj: strIndexed){
+        memObj.second->~memcell();
+    }
+
+    strIndexed.clear();
+
+
+    for (auto &memObj: numIndexed){
+        memObj.second->~memcell();
+    }
+
+    numIndexed.clear();
+}
