68d7a35edce16e4ac72c5ac2f804faa5ccdfaba3
konstantina papafragkaki <csd4331@csd.uoc.gr>
2022-06-03 00:32:57 +0300

    implement set-get of number memcell

--- memory/memcell/memcell.cpp
+++ memory/memcell/memcell.cpp
@@ -1,3 +1,10 @@
 #include "memory/memcell/memcell.hpp"
 
 static memcell *assign(memcell *&lv, memcell *rv);
+
+void numberMemcell::setNumber(const double number) {
+    this->value = number;
+}
+double numberMemcell::getNumber(void) const override {
+    return this->value;
+}
