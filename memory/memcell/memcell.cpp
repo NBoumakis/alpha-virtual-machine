30de8b7a87fd8f82dc83a522ea247bcd252ba0b5
konstantina papafragkaki <csd4331@csd.uoc.gr>
2022-06-03 00:34:53 +0300

    implement get type-name of number memcell

--- memory/memcell/memcell.cpp
+++ memory/memcell/memcell.cpp
@@ -5,6 +5,15 @@ static memcell *assign(memcell *&lv, memcell *rv);
 void numberMemcell::setNumber(const double number) {
     this->value = number;
 }
+
 double numberMemcell::getNumber(void) const override {
     return this->value;
+}
+
+memcell_type numberMemcell::getType(void) const override {
+    return memcell_type::number_m;
+}
+
+std::string numberMemcell::getTypeName() const override {
+    return "number";
 }
