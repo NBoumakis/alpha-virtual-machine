d3ae6a2dc3aede6653578eef5ccc529bf5191d16
konstantina papafragkaki <csd4331@csd.uoc.gr>
2022-06-02 12:21:52 +0300

    set-get elements in dynamic_tables

--- table/dynamic_table.cpp
+++ table/dynamic_table.cpp
@@ -19,11 +19,37 @@ dynamic_table::~dynamic_table(){
     numIndexed.clear();
 }
 
-memcell *dynamic_table::get_elem(const memcell *) const{
+memcell *dynamic_table::get_elem(const memcell *key) const{
+    switch (key->type){
     
+    case number_m:
+        return numIndexed.at(key);
+
+    case string_m:
+        return strIndexed.at(key);
+    
+    
+    default:
+        assert(false);
+    }
 }
 
 void dynamic_table::set_elem(const memcell *key, const memcell *value){
+    
+    switch (key->type){
+    
+    case number_m:
+        numIndexed.insert(std::pair<std::string,memcell *>(key,value));
+        break;
+
+    case string_m:
+         strIndexed.insert(std::pair<std::string,memcell *>(key,value));
+        break;
+    
+    
+    default:
+        assert(false);
+    }
     
 }
 
