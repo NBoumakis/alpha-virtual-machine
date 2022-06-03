05f589a287d3aa5344a1b1c9f5282c468735c316
Lionta Katerina <csd4426@csd.uoc.gr>
2022-06-03 11:12:47 +0300

    execute assign

---
+++ executer/execute/execute_funcs_assign.cpp
@@ -0,0 +1,13 @@
+#include "execute_funcs_assign.hpp"
+#include "cpu.hpp"
+#include "decode.hpp"
+#include <assert.h>
+
+void execute_assign(instruction *instr) {
+    memcell *lv = translate_operand(instr->result, (memcell *)nullptr);
+    memcell *rv = translate_operand(instr->arg1, cpu::ax);
+
+    assert(lv && rv);
+
+    assing(lv, rv);
+}
