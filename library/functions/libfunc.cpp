6038a3d6d6e5ac5514719a8138ff8a677cbcf5d3
Lionta Katerina <csd4426@csd.uoc.gr>
2022-06-03 20:03:41 +0300

    typeof(),totalarguments()

--- library/functions/libfunc.cpp
+++ library/functions/libfunc.cpp
@@ -1,24 +1,33 @@
-#include <cpu.hpp>
-#include <memory/memcell/memcell.hpp>
+#include "executer/cpu.hpp"
+#include "memory/memcell/memcell.hpp"
 
-void libfunc_typeof(){
+void libfunc_typeof() {
     unsigned long num_of_actuals = cpu::env.total_actuals();
 
-    if (num_of_actuals != 1){
-        //error one argument not expected in 'typeof'
+    if (num_of_actuals != 1) {
+        // error one argument not expected in 'typeof'
 
-    }else{
-       delete cpu::retval;
-       cpu::retval = new string(cpu::env->get_actual(0UL).get_type_name());
+    } else {
+        delete cpu::retval;
+        cpu::retval = new stringMemcell((cpu::env)->get_actual(0UL).getTypeName());
     }
 }
 
-void libfunc_print(){
+void libfunc_print() {
     unsigned long num_of_actuals = cpu::env.total_actuals();
 
-    for(unsigned long i = 0 ; i < num_of_actuals ; i++){
-        std::string s = static_cast<std::string> (*cpu::env.get_actual(i));
+    for (unsigned long i = 0; i < num_of_actuals; i++) {
+        std::string s = static_cast<std::string>(*cpu::env.get_actual(i));
     }
-
 }
+void libfunc_totalarguments() {
+    unsigned long p_topsp = cpu::env.get_envvalue(cpu::topsp + 1); // AVM_SAVEDTOPSP_OFFSET
+    delete cpu::retval;
 
+    if (!p_topsp) {
+        // avm_error:totalarguments called outside a function
+        cpu::retval = new nilMemcell();
+    } else {
+        cpu::retval = new numberMemcell(cpu::env.get_envvalue(p_topsp + 4)); // AVM_NUMACTUALS_OFFSET
+    }
+}
