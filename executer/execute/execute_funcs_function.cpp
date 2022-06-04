3fbbe61f4e28859ce5106ce8040a35a14977566f
Lionta Katerina <csd4426@csd.uoc.gr>
2022-06-04 20:36:02 +0300

    fixed include  paths

--- executer/execute/execute_funcs_function.cpp
+++ executer/execute/execute_funcs_function.cpp
@@ -1,7 +1,7 @@
-#include "execute_funcs_function.hpp"
-#include "cpu.hpp"
-#include "environment.hpp"
-#include "memcell.hpp"
+#include "executer\execute\execute_funcs_function.hpp"
+#include "executer\cpu.hpp"
+#include "library\environment\environment.hpp"
+#include "memory\memcell\memcell.hpp"
 #include <assert.h>
 #include <iostream>
 
