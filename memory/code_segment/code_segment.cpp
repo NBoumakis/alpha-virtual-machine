a458dd81e2d1a42624f375d5025ce94cff4f68d6
Lionta Katerina <csd4426@csd.uoc.gr>
2022-06-04 13:34:23 +0300

    add insert

--- memory/code_segment/code_segment.cpp
+++ memory/code_segment/code_segment.cpp
@@ -7,3 +7,7 @@ unsigned long code_segment::size() {
 instruction &code_segment::operator[](unsigned long i) {
     return instructions.at(i);
 }
+
+void code_segment::insert(instruction &instr) {
+    instructions.push_back(instr);
+}
