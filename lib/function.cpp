5e2dba789650f94bcf7c9d2415e57eeca56e8835
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-06-04 18:44:29 +0300

    Userfunction class debloat

--- lib/function.cpp
+++ lib/function.cpp
@@ -1,70 +1,16 @@
-#include "lib/symbol.h"
+#include "lib/function.hpp"
 #include <cassert>
 #include <string>
-
-Symbol::Symbol(std::string name, unsigned int scope, unsigned int line, unsigned int funcDepth, SymbolType type)
-    : name(name), scope(scope), line(line), funcDepth(funcDepth), isActive(true), type(type) {}
-
-Symbol::Symbol(std::string name, unsigned int scope, unsigned int line, unsigned int funcDepth, SymbolType type, bool isActive)
-    : name(name), scope(scope), line(line), funcDepth(funcDepth), isActive(isActive), type(type) {}
-
-std::string Variable::to_string() {
-    std::string result;
-
-    result += "\"" + name + "\"\t";
-    result += "[" + type_string() + "]\t\t";
-
-    result += "(line " + std::to_string(line) + ")\t";
-    result += "(scope " + std::to_string(scope) + ")";
-    result += "(offset " + std::to_string(offset) + ")";
-
-    return result;
-}
-
-std::string Variable::type_string() {
-    assert(this);
-
-    switch (this->space) {
-    case GLOBAL_VAR:
-        return "global variable";
-
-    case LOCAL_VAR:
-        return "local variable";
-
-    case FORMAL_ARG:
-        return "formal argument";
-
-    default:
-        assert(false);
-    }
-}
 
 std::string Function::to_string() {
     std::string result;
 
-    result += "\"" + name + "\"\t";
+    result += "\"" + id + "\"\t";
 
-    result += "[" + type_string() + "]\t\t";
+    result += "[userfunction]\t\t";
 
-    result += "(line " + std::to_string(line) + ")\t";
-    result += "(scope " + std::to_string(scope) + ")";
+    result += "(address " + std::to_string(taddress) + ")\t";
+    result += "(local size " + std::to_string(totalLocals) + ")";
 
     return result;
 }
-
-std::string Function::type_string() {
-    assert(this);
-
-    switch (this->type) {
-    case USER_FUNC:
-        return "user function";
-
-    case LIB_FUNC:
-        return "library function";
-
-    default:
-        assert(false);
-    }
-}
-
-Symbol::~Symbol() {}
--- lib/function.hpp
+++ lib/function.hpp
@@ -3,69 +3,18 @@
 
 #include <string>
 
-enum SymbolType {
-    VARIABLE,
-    USER_FUNC,
-    LIB_FUNC
-};
-
-enum ScopespaceType {
-    GLOBAL_VAR,
-    LOCAL_VAR,
-    FORMAL_ARG
-};
-
-class Symbol {
+class Function {
 public:
-    std::string name;
-    unsigned int scope;
-    unsigned int line;
-    unsigned int funcDepth;
-    bool isActive;
-
-    SymbolType type;
-
-    friend class Scope;
-
-public:
-    Symbol(std::string name, unsigned int scope, unsigned int line, unsigned int funcDepth, SymbolType type);
-    Symbol(std::string name, unsigned int scope, unsigned int line, unsigned int funcDepth, SymbolType type, bool isActive);
-
-    virtual std::string to_string() = 0;
-    virtual std::string type_string() = 0;
-
-    ~Symbol();
-};
-
-class Variable : public Symbol {
-public:
-    unsigned long offset;
-    ScopespaceType space;
-
-public:
-    Variable(std::string name, unsigned int scope, unsigned int line, unsigned int funcDepth, ScopespaceType type, unsigned long offset)
-        : Symbol(name, scope, line, funcDepth, VARIABLE), offset(offset), space(type) {}
-    Variable(std::string name, unsigned int scope, unsigned int line, unsigned int funcDepth, ScopespaceType type, unsigned long offset, bool isActive)
-        : Symbol(name, scope, line, funcDepth, VARIABLE, isActive), offset(offset), space(type) {}
-
-    std::string to_string() override;
-    std::string type_string() override;
-};
-
-class Function : public Symbol {
-public:
-    unsigned long iaddress;
+    std::string id;
     unsigned long taddress;
     unsigned long totalLocals;
 
 public:
-    Function(std::string name, unsigned int scope, unsigned int line, unsigned int funcDepth, SymbolType type, unsigned long offset, unsigned long iaddress)
-        : Symbol(name, scope, line, funcDepth, type), iaddress(iaddress) {}
-    Function(std::string name, unsigned int scope, unsigned int line, unsigned int funcDepth, SymbolType type, unsigned long offset, unsigned long iaddress, bool isActive)
-        : Symbol(name, scope, line, funcDepth, type, isActive), iaddress(iaddress) {}
+    Function(std::string &id, unsigned long taddress, unsigned long localSize)
+        : taddress(taddress), id(id), totalLocals(localSize) {}
 
-    std::string to_string() override;
-    std::string type_string() override;
+    std::string to_string();
+    std::string type_string();
 };
 
 #endif /* __SYMBOL_H */
