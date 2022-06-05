820c653a20b42b862711d72c929546ff5e62be77
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-06-06 01:18:46 +0300

    execute_cycle

--- executer/cpu.cpp
+++ executer/cpu.cpp
@@ -8,3 +8,25 @@ code_segment cpu::code;
 const_values cpu::pools;
 bool cpu::execution_finished;
 execute cpu::executer;
+
+void cpu::cycle() {
+    if (cpu::execution_finished)
+        return;
+
+    if (cpu::pc == cpu::code_size) {
+        execution_finished = true;
+        return;
+    } else {
+        assert(cpu::pc < cpu::code_size);
+
+        instruction *instr = cpu::code[pc];
+        assert(instr &&
+               instr->opcode >= 0 &&
+               instr->opcode <= vmopcode::nop_vmiop);
+
+        unsigned long oldPC = pc;
+        cpu::executer[instr->opcode](instr);
+        if (oldPC == pc)
+            ++pc;
+    }
+}
