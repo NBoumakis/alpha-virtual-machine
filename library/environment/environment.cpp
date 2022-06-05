3b38d08f7e4197a7788bcda5c389f232b2814ec1
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-06-05 17:05:44 +0300

    call_libfunc

--- library/environment/environment.cpp
+++ library/environment/environment.cpp
@@ -7,3 +7,22 @@ void environment::call_save_environment() {
     cpu::stack.push(new numberMemcell(cpu::top + total_actuals + 2));
     cpu::stack.push(new numberMemcell(cpu::topsp));
 }
+
+void environment::call_libfunc(const std::string &id) {
+    std::function<void()> func;
+
+    try {
+        func = dispatch.at(id);
+
+        cpu::topsp = cpu::top;
+        total_actuals = 0;
+
+        func();
+
+        if (!cpu::execution_finished)
+            cpu::executer[funcexit_vmiop](nullptr);
+    } catch (const std::out_of_range &e) {
+        std::cerr << "ERROR: unsupported lib function " << id << " called!" << std::endl;
+        cpu::execution_finished = true;
+    }
+}
