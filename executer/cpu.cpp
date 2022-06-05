62354539ecf5584bdf0d863b38f4cbbdf565a596
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-06-05 12:54:37 +0300

    CPU static members definitions

---
+++ executer/cpu.cpp
@@ -0,0 +1,9 @@
+#include "executer/cpu.hpp"
+
+unsigned long cpu::pc, cpu::topsp, cpu::top, cpu::code_size;
+memcell *cpu::ax, *cpu::bx, *cpu::cx, *cpu::retval;
+program_stack cpu::stack;
+environment cpu::env;
+code_segment cpu::code;
+const_values cpu::pools;
+char cpu::execution_finished;
