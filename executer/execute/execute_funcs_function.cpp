7501057ed4e4d14088dec3ec6103bf358cfaa2a0
Lionta Katerina <csd4426@csd.uoc.gr>
2022-06-05 20:49:59 +0300

    add & for translate operand

--- executer/execute/execute_funcs_function.cpp
+++ executer/execute/execute_funcs_function.cpp
@@ -7,7 +7,7 @@
 #include <iostream>
 
 void execute_call(instruction *instr) {
-    memcell *func = translate_operand(instr->arg1, cpu::ax);
+    memcell *&func = translate_operand(instr->arg1, cpu::ax);
     assert(func);
 
     cpu::env.call_save_environment();
@@ -40,7 +40,7 @@ void execute_call(instruction *instr) {
 }
 
 void execute_pusharg(instruction *instr) {
-    memcell *arg = translate_operand(instr->arg1, cpu::ax);
+    memcell *&arg = translate_operand(instr->arg1, cpu::ax);
     assert(arg);
 
     cpu::stack.push(arg);
@@ -50,7 +50,7 @@ void execute_pusharg(instruction *instr) {
 }
 
 void execute_funcenter(instruction *instr) {
-    memcell *func = translate_operand(instr->arg1, cpu::ax);
+    memcell *&func = translate_operand(instr->arg1, cpu::ax);
     assert(func);
     assert(cpu::pc == func->getUserFunc());
 
