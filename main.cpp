a5613eb71c1bfe37cb75666924f8da77d4ddb370
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-06-06 14:03:26 +0300

    register implemented libfuncs

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "executer/cpu.hpp"
+#include "library/functions/libfunc.hpp"
 #include "loader/loader.hpp"
 #include <fstream>
 
@@ -6,6 +7,10 @@ int main() {
     std::ifstream in_file("bin.abc");
 
     load_text(in_file);
+
+    cpu::env.register_libfunc("print", libfunc_print);
+    cpu::env.register_libfunc("totalarguments", libfunc_totalarguments);
+    cpu::env.register_libfunc("typeof", libfunc_typeof);
 
     while (!cpu::execution_finished) {
         cpu::cycle();
