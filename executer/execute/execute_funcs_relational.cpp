f0701b9d7aae59ecde8cb713b5a119485e2c3eb8
konstantina papafragkaki <csd4331@csd.uoc.gr>
2022-06-08 17:58:25 +0300

    fix errors in not equal execution

--- executer/execute/execute_funcs_relational.cpp
+++ executer/execute/execute_funcs_relational.cpp
@@ -49,7 +49,7 @@ void execute_jne(instruction *instr) {
 
     if (rv1->getType() == memcell_type::undefined_m || rv2->getType() == memcell_type::undefined_m) {
         // FIXME
-        std::cerr << "ERROR: 'undef' involved in equality!";
+        std::cerr << "ERROR: cannot perform not equal operation in an undefined operand!";
         cpu::execution_finished = true;
     } else if (rv1->getType() == memcell_type::nil_m || rv2->getType() == memcell_type::nil_m) {
         result = (rv1->getType() == memcell_type::nil_m) && (rv2->getType() == memcell_type::nil_m);
@@ -57,7 +57,7 @@ void execute_jne(instruction *instr) {
         result = (static_cast<bool>(*rv1) != static_cast<bool>(*rv2));
     } else if (rv1->getType() != rv2->getType()) {
         // FIXME
-        std::cerr << "ERROR: " << rv1->getTypeName() << " != " << rv2->getTypeName() << "is illegal!";
+        std::cerr << "ERROR: cannot perform not equal operation between " << rv1->getTypeName() << " and " << rv2->getTypeName() << "!";
         cpu::execution_finished = true;
     } else {
         result = (*rv1 != *rv2);
