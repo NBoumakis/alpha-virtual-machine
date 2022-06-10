c78eda7cb11995a1ad075fa266cdaa78686ae5d0
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-06-10 17:35:11 +0300

    Command-line arguments for main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "executer/cpu.hpp"
 #include "library/functions/libfunc.hpp"
 #include "loader/loader.hpp"
+#include <algorithm>
 #include <fstream>
 
 void init_libfuncs() {
@@ -18,10 +19,53 @@ void init_libfuncs() {
     cpu::env.register_libfunc("argument", libfunc_argument);
 }
 
-int main() {
-    std::ifstream in_file("bin.abc");
+char *getCmdOption(char **begin, char **end, const std::string &option) {
+    char **itr = std::find(begin, end, option);
+    if (itr != end && ++itr != end) {
+        return *itr;
+    }
+    return nullptr;
+}
 
-    load_text(in_file);
+bool cmdOptionExists(char **begin, char **end, const std::string &option) {
+    return std::find(begin, end, option) != end;
+}
+
+int main(int argc, char *argv[]) {
+    char *in_fname = getCmdOption(argv, argv + argc, "--in");
+    bool is_text = cmdOptionExists(argv, argv + argc, "--text");
+
+    if (in_fname) {
+        if (is_text) {
+            std::ifstream in_file(in_fname);
+
+            if (!in_file.is_open()) {
+                std::cerr << "Cannot open file " << in_fname << " for reading." << std::endl;
+                return -1;
+            }
+
+            if (!load_text(in_file)) {
+                std::cerr << "Error loading text-formatted executable " << in_fname << "." << std::endl;
+                return -1;
+            }
+        } else {
+            std::ifstream in_file(in_fname, std::ios::in | std::ios::binary);
+
+            if (!in_file.is_open()) {
+                std::cerr << "Cannot open file " << in_fname << " for reading." << std::endl;
+                return -1;
+            }
+
+            if (!load_text(in_file)) {
+                std::cerr << "Error loading binary executable " << in_fname << "." << std::endl;
+                return -1;
+            }
+        }
+    } else {
+        std::cerr << "No argument found for mandatory option --in." << std::endl;
+
+        return -1;
+    }
 
     init_libfuncs();
 
