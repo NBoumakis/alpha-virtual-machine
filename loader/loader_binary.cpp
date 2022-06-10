13d616819208274fa1ffefd014df81285a1da55f
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-06-10 16:29:56 +0300

    strings and inserters for binary

--- loader/loader_binary.cpp
+++ loader/loader_binary.cpp
@@ -25,8 +25,40 @@ static bool globals(std::ifstream &in_file) {
     return in_file.good();
 }
 
+static bool strings(std::ifstream &in_file, std::function<void(unsigned long, std::string &)> insert) {
+    unsigned long number;
+
+    in_file.read(reinterpret_cast<char *>(&number), sizeof(number));
+
+    bool load_success = in_file.good();
+
+    for (unsigned long i = 0; i < number && load_success; ++i) {
+        std::string res;
+        load_success = string(in_file, res);
+        if (load_success) {
+            insert(i, res);
+        }
+    }
+}
+
+static void insert_string_array(unsigned long index, std::string &str) {
+    cpu::pools.insert_string(index, str);
+}
+
+static void insert_number_array(unsigned long index, double num) {
+    cpu::pools.insert_number(index, num);
+}
+
+static void insert_userfunc_array(unsigned long index, Function *func) {
+    cpu::pools.insert_userfunc(index, func);
+}
+
+static void insert_libfunc_array(unsigned long index, std::string &id) {
+    cpu::pools.insert_libfunc(index, id);
+}
+
 static bool arrays(std::ifstream &in_file) {
-    return strings(in_file) && numbers(in_file) && userfuncs(in_file) && libfuncs(in_file);
+    return strings(in_file, insert_string_array) && numbers(in_file, insert_number_array) && userfuncs(in_file, insert_userfunc_array) && libfuncs(in_file, insert_libfunc_array);
 }
 
 static bool avmbinaryfile(std::ifstream &in_file) {
